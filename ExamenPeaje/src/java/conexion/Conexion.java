3e8898e63bb4e273c537298eeff2fa0eb222e470
Reveco <Reveco@ntb-reveco>
2017-12-05 23:07:02 -0300

    Revert "Revert "agrego clases""
    This reverts commit f3302d6c1ef942ca95f794a1019f8a5df0501dfe.

---
+++ ExamenPeaje/src/java/conexion/Conexion.java
@@ -0,0 +1,47 @@
+
+package conexion;
+
+import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+
+
+/**
+ *
+ * @author Francisco Benitez
+ */
+public class Conexion {
+    public static Conexion instancia;
+    private Connection cnn;
+        
+    public Conexion(){
+        String driver = "com.mysql.jdbc.Driver";
+        String url ="jdbc:mysql://localhost:3306/perufrank";
+        String usu ="root";
+        String pass ="";
+        
+        try {
+            Class.forName(driver);
+            cnn = DriverManager.getConnection(url, usu, pass);
+        } catch (Exception ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        
+    }
+    public static Conexion getInstance(){
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+    
+    public void cerrar(){
+        instancia = null;
+    }
+        
+    
+    public Connection getCnn(){
+        return cnn;
+    }
+}
